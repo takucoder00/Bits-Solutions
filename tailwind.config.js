a420c71a63e1eac3ea194afcea672c7f5bd589b3
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-10-20 10:45:33 +0200

    ✨  Add new assets and modify configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        orange:{
+          50: '#fff7ed',
+          100: '#ffedd5',
+          200: '#fed7aa',
+          300: '#fdba74',
+          400: '#fb923c',
+          500: '#f97316',
+          600: '#ea58oc',
+          700: '#c241oc',
+          800: '#9a3412',
+          900: '#7c2d12',
+        },
+        gray: {
+          custom: "#d8d9db"
+        },
+      }
+    },
   },
   variants: {
     extend: {
