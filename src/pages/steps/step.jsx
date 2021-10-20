7bf2e5bb6b2286574e9741e005eb489034748d76
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-10-20 10:46:07 +0200

    📝  Add steps section

--- src/pages/steps/step.jsx
+++ src/pages/steps/step.jsx
@@ -4,72 +4,46 @@ import React from "react";
 
 export const Steps = () => {
     return (
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="px-4 py-16 mb-20 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
 
           <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-            <span className="relative inline-block">
-        
-              <span className="relative">The</span>
-            </span>{' '}
-            quick, brown fox jumps over a lazy dog
+            Get our services in 3 easy steps
           </h2>
           <p className="text-base text-gray-700 md:text-lg">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
+            At Bitssolutions we focus on providing the best services to our clients in 3 easy steps. Simplicity is key.
           </p>
         </div>
         <div className="grid gap-8 row-gap-8 lg:grid-cols-3">
           <div className="sm:text-center">
-            <div className="flex items-center justify-center w-24 h-24 mb-4 text-5xl font-extrabold rounded-full text-deep-purple-accent-400 bg-indigo-50 sm:mx-auto">
+            <div className="flex items-center justify-center w-24 h-24 mb-4 text-5xl font-extrabold rounded-full text-deep-purple-accent-400 bg-orange-100 sm:mx-auto">
               1
             </div>
-            <h6 className="mb-2 font-semibold leading-5">Slugging catcher</h6>
-            <p className="max-w-md mb-3 text-sm text-gray-900 sm:mx-auto">
-              Lookout flogging bilge rat main sheet bilge water nipper fluke to go
-              on account heave down clap of thunder
+            <h6 className="mb-2 leading-5 text-xl font-medium text-gray-900">Ask for a service</h6>
+            <p className="max-w-md mb-3 sm:mx-auto text-base text-center text-gray-500">
+              Pick any of our services that you need.
             </p>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-            >
-              Learn more
-            </a>
+           
           </div>
           <div className="sm:text-center">
-            <div className="flex items-center justify-center w-24 h-24 mb-4 text-5xl font-extrabold rounded-full text-deep-purple-accent-400 bg-indigo-50 sm:mx-auto">
+            <div className="flex items-center justify-center w-24 h-24 mb-4 text-5xl font-extrabold rounded-full text-deep-purple-accent-400 bg-orange-200 sm:mx-auto">
               2
             </div>
-            <h6 className="mb-2 font-semibold leading-5">Do your best</h6>
-            <p className="max-w-md mb-3 text-sm text-gray-900 sm:mx-auto">
-              They urge you to put down your sword and come join the winners. In
-              22 years the only 'winners'
+            <h6 className="mb-2 leading-5 text-xl font-medium text-gray-900">Get a quote</h6>
+            <p className="max-w-md mb-3 sm:mx-auto text-base text-center text-gray-500">
+              Request a quote from our team and make payment.
             </p>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-            >
-              Learn more
-            </a>
+            
           </div>
           <div className="sm:text-center">
-            <div className="flex items-center justify-center w-24 h-24 mb-4 text-5xl font-extrabold rounded-full text-deep-purple-accent-400 bg-indigo-50 sm:mx-auto">
+            <div className="flex items-center justify-center w-24 h-24 mb-4 text-5xl font-extrabold rounded-full text-deep-purple-accent-400 bg-orange-300 sm:mx-auto">
               3
             </div>
-            <h6 className="mb-2 font-semibold leading-5">Be yourself</h6>
-            <p className="max-w-md mb-3 text-sm text-gray-900 sm:mx-auto">
-              Tell them I hate them. Is the Space Pope reptilian!? Tell her she
-              looks thin. Hello, little man
+            <h6 className="mb-2 leading-5 text-xl font-medium text-gray-900">We deliver</h6>
+            <p className="max-w-md mb-3 sm:mx-auto text-base text-center text-gray-500">
+              We deliver the service right to your door.
             </p>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-            >
-              Learn more
-            </a>
+        
           </div>
         </div>
       </div>
