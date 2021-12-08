cc05298564f817b3734e880f49d215ccd1319c86
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-12-08 11:42:51 +0200

    Add business area component

---
+++ components/businessSystems/businessAreas.tsx
@@ -0,0 +1,95 @@
+import React from 'react'
+
+export default function BusinessAreas() {
+    return (
+        <div>
+            
+
+
+    <div className="bg-orange-50 dark:bg-gray-800">
+
+        <div className="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-128 lg:py-16 lg:flex-row lg:items-center">
+            <div className="w-full lg:w-1/2">
+                <div className="lg:max-w-lg">
+                    <h1 className="text-2xl font-medium tracking-wide text-gray-800 dark:text-white lg:text-4xl">Areas Covered</h1>
+                    <p className="mt-2 text-gray-600 dark:text-gray-300">Our standard business systems covers a number of areas which include:</p>
+                    <div className="grid gap-6 mt-8 sm:grid-cols-2">
+                        <div className="flex items-center text-gray-800 -px-3 dark:text-gray-200">
+                            <svg className="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+
+                            <span className="mx-3">Professionalism</span>
+                        </div>
+
+                        <div className="flex items-center text-gray-800 -px-3 dark:text-gray-200">
+                            <svg className="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+
+                            <span className="mx-3">Marketing</span>
+                        </div>
+
+                        <div className="flex items-center text-gray-800 -px-3 dark:text-gray-200">
+                            <svg className="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+
+                            <span className="mx-3">Business Strategies</span>
+                        </div>
+
+                        <div className="flex items-center text-gray-800 -px-3 dark:text-gray-200">
+                            <svg className="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+
+                            <span className="mx-3">Innovative Ideas</span>
+                        </div>
+
+                        <div className="flex items-center text-gray-800 -px-3 dark:text-gray-200">
+                            <svg className="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+
+                            <span className="mx-3">Networking</span>
+                        </div>
+
+                        <div className="flex items-center text-gray-800 -px-3 dark:text-gray-200">
+                            <svg className="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+
+                            <span className="mx-3">Business Support Services</span>
+                        </div>
+                        <div className="flex items-center text-gray-800 -px-3 dark:text-gray-200">
+                            <svg className="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+
+                            <span className="mx-3">Standard Operating Procedures</span>
+                        </div>
+                        <div className="flex items-center text-gray-800 -px-3 dark:text-gray-200">
+                            <svg className="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>
+
+                            <span className="mx-3">Many Other Things</span>
+                        </div>
+   
+                    </div>
+                    <p className="mt-2  text-gray-600">
+                    Our members should aim to meet our standards in order to enhance their business performance.
+
+                    </p>
+        
+                </div>
+            </div>
+    
+            <div className="flex items-center justify-center w-full h-96 lg:w-1/2">
+                <img className="object-cover w-full h-full max-w-2xl rounded-md" src="https://images.pexels.com/photos/10376257/pexels-photo-10376257.jpeg?cs=srgb&dl=pexels-rodnae-productions-10376257.jpg&fm=jpg" alt="glasses photo"/>
+            </div>
+        </div>
+    </div>
+        </div>
+    )
+}
