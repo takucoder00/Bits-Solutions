dde2047fb5aa43a4e008aec98cbb02c0e79d4ac4
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-12-08 12:41:23 +0200

    Change color

--- components/about/aboutSection.tsx
+++ components/about/aboutSection.tsx
@@ -28,7 +28,7 @@ export default function AboutSection() {
           <div className="flex flex-col justify-center">
             <div className="flex">
               <div className="mr-4">
-                <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-indigo-50">
+                <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-orange-100">
                   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                 </div>
               </div>
@@ -44,7 +44,7 @@ export default function AboutSection() {
             </div>
             <div className="flex">
               <div className="mr-4">
-                <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-indigo-50">
+                <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-orange-100">
                   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"></path></svg>
                 </div>
               </div>
@@ -58,7 +58,7 @@ export default function AboutSection() {
             </div>
             <div className="flex">
               <div className="mr-4">
-                <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-indigo-50">
+                <div className="flex items-center justify-center w-10 h-10 mb-3 rounded-full bg-orange-100">
                   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
                 </div>
               </div>
